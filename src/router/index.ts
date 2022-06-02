e833649cf52a3108aa8b99844230814ee83e9d12
Vincent Courtois <courtois.vincent@outlook.com>
2022-06-02 23:47:09 +0200

    Add the routes and empty components for stages list and view

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,20 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import StagesList from '../views/stages/List.vue'
+import StageDetails from '../views/stages/Details.vue'
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Stages list',
+    component: StagesList
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/:id',
+    name: 'Stage details',
+    component: StageDetails
   }
 ]
 
