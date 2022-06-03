17944d333f53ffaad0e7e8837571d6fc20c58b78
Vincent Courtois <courtois.vincent@outlook.com>
2022-06-03 12:32:53 +0200

    Make the stages list into a neat table

--- src/views/stages/List.vue
+++ src/views/stages/List.vue
@@ -6,6 +6,15 @@ import { namespace } from 'vuex-class'
 
 @Component
 export default class StagesList extends Vue {
+
+  private headers = [
+    {text: 'ID', value: 'id'},
+    {text: 'Name', value: 'name'},
+    {text: 'Nodes', value: 'nodes'},
+    {text: 'Links', value: 'links'},
+    {text: 'Actions', value: 'actions'},
+  ]
+
   @namespace('stages').State('list') stages!: IStage[];
 
   @namespace('stages').Action(StageActionTypes.FETCH_LIST) fetchlist: any;
@@ -18,8 +27,14 @@ export default class StagesList extends Vue {
 
 <template>
   <v-container>
-    <v-row>
-      {{ stages }}
-    </v-row>
+    <v-layout child-flex>
+      <v-data-table fluid :headers="headers" :items="stages" class="elevation-1">
+        <template v-slot:item.actions="{ item }">
+          <v-btn icon :to="`/${item.id}`">
+            <v-icon>mdi-eye</v-icon>
+          </v-btn>
+        </template>
+      </v-data-table>
+    </v-layout>
   </v-container>
 </template>
