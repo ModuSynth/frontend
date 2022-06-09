003d27577c76ee2e2f81767d2055dd0f23ef18f1
Vincent Courtois <vincent.courtois@coddity.com>
2022-06-09 13:49:43 +0200

    Use the mutations in the state to make the dragndrop work

--- src/store/nodes/mutations.ts
+++ src/store/nodes/mutations.ts
@@ -7,26 +7,27 @@ const mutations: MutationTree<INodeState> & NodeMutations = {
   [NodeMutationTypes.SET_NODES_LIST](state, payload) {
     state.nodes = payload;
   },
-  [NodeMutationTypes.START_DRAG](state, {node, x, y}) {
+  [NodeMutationTypes.START_DRAG](state, {node, $event}) {
     state.dragged = node;
-    state.dragOrigin = { x, y }
+    state.dragOrigin = { x: $event.clientX, y: $event.clientY }
   },
-  [NodeMutationTypes.MOVE_DRAG](state, event) {
+  [NodeMutationTypes.MOVE_DRAG](state, $event) {
     if (state.dragged === undefined) return;
 
     const delta: ICoordinates = {
-      x: event.clientX - state.dragOrigin.x,
-      y: event.clientY - state.dragOrigin.y
+      x: $event.clientX - state.dragOrigin.x,
+      y: $event.clientY - state.dragOrigin.y
     }
     state.dragged.x += delta.x;
     state.dragged.y += delta.y;
     state.dragOrigin = {
-      x: event.clientX,
-      y: event.clientY
+      x: $event.clientX,
+      y: $event.clientY
     }
   },
   [NodeMutationTypes.END_DRAG](state) {
     state.dragged = undefined;
+    state.dragOrigin = { x: 0, y: 0 };
   }
 }
 
