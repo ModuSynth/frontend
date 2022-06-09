3f3b7fe706735e7c2176cfcdc3d5e4e82e101bbe
Vincent Courtois <vincent.courtois@coddity.com>
2022-06-09 13:28:16 +0200

    Add necessary variables in the state

--- src/store/nodes/interfaces.ts
+++ src/store/nodes/interfaces.ts
@@ -1,8 +1,14 @@
 import INode from "@/interfaces/INode";
+import ICoordinates from "@/interfaces/utils/ICoordinates";
 import { NodeMutationTypes } from "./enums";
 
 export interface INodeState {
-  nodes: INode[]
+  // The array of the nodes in the currently displayed stage.
+  nodes: INode[];
+  // The node currently being dragged on the stage.
+  dragged?: INode;
+  // The coordinates from the last drag event on the dragged node.
+  dragOrigin: ICoordinates;
 }
 
 export type NodeMutations<S = INodeState> = {
