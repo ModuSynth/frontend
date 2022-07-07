e5784e3054ecd556374a0ddc72619f13b40ef63e
Vincent Courtois <vincent.courtois@coddity.com>
2022-07-07 17:20:09 +0200

    Add a default value to type

--- src/utils/defaults.ts
+++ src/utils/defaults.ts
@@ -1,5 +1,5 @@
 const defaults: {[key: string]: any} = {
-  "OscillatorNode": {"frequency": 440, "detune": 0},
+  "OscillatorNode": {"frequency": 440, "detune": 0, "type": "sine"},
   "GainNode": {"gain": 0.1},
   "OutputNode": {}
 }
